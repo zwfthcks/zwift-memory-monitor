be8ac59e207c58948a6b5181dbe240c7beda2c4d
Jesper Rosenlund Nielsen <16505099+jeroni7100@users.noreply.github.com>
2023-06-15 18:21:40 +0200

    1.42 pattern

--- src/lookup.js
+++ src/lookup.js
@@ -9,6 +9,48 @@ module.exports =
     playerstate: [
         {
             // version: version numbers matching the pattern and positions
+            versions: ">=1.42.0",
+            // offsets: field configuration
+            offsets: {
+                // Relative position to player (the baseaddress)
+                // Here calculated as the field specific offset used in MOV minus 0x20 (offset for player in MOV)
+                climbing: [0x60 - 0x20, 'uint32'],
+                speed: [0x3c - 0x20, 'uint32'],
+                distance: [0x30 - 0x20, 'uint32'],
+                time: [0x64 - 0x20, 'uint32'],
+                cadence_uHz: [0x48 - 0x20, 'uint32'], // unit uHz
+                heartrate: [0x50 - 0x20, 'uint32'],
+                power: [0x54 - 0x20, 'uint32'],
+                player: [0x20 - 0x20, 'uint32'],
+                x: [0x88 - 0x20, 'float'], 
+                y: [0xa0 - 0x20, 'float'], 
+                altitude: [0x8c - 0x20, 'float'], 
+                watching: [0x90 - 0x20, 'uint32'],
+                world: [0x110 - 0x20, 'uint32'],
+                work: [ 0x84 - 0x20, 'uint32' ],  // unit mWh
+            },
+            // signatures: patterns to search for
+            signatures: [
+                {
+                    pattern: '<player> 00 00 00 00 ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 00 00 00 00 00 00 00 00 <player> 00 00 00 00', // matches 1.39 in all cases (and hopefully next versions, too). No more than 1-2 sec. slower than next pattern.
+                    addressOffset: 8+16*4+8 // baseaddress offset to start of found pattern
+                },
+                {
+                    pattern: '<player> 00 00 00 00 ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 00 00 00 00 00 00 00 00 <player> 00 00 00 00', // matches 1.39 in all cases (and hopefully next versions, too). No more than 1-2 sec. slower than next pattern.
+                    addressOffset: 8+18*4+8 // baseaddress offset to start of found pattern
+                },
+                {
+                    pattern: 'EF 00 00 00 00 00 00 00 00 00 00 00 <player> 00 00 00 00', // matches 1.39 when started riding
+                    addressOffset: 12 // baseaddress offset to start of found pattern
+                },
+                {
+                    pattern: '6F 00 00 00 00 00 00 00 00 00 00 00 <player> 00 00 00 00', // matches 1.39 while in initial screens
+                    addressOffset: 12 // baseaddress offset to start of found pattern
+                },
+            ],
+        },
+        {
+            // version: version numbers matching the pattern and positions
             versions: ">=1.39.0",
             // offsets: field configuration
             offsets: {
