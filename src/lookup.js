55e14337468ba9edb5e6dd031d6e45a2b54ec7aa
Jesper Rosenlund Nielsen <16505099+jeroni7100@users.noreply.github.com>
2025-09-03 15:05:01 +0200

    fix: correct mustBeGreaterThanEqual constraints in player profile patterns

--- src/lookup.js
+++ src/lookup.js
@@ -207,12 +207,12 @@ const patterns =
                     rules: {
                         mustBeVariable: [
                             [ 15 * 4, 'uint32', '<jersey>' ],
-                        ]
-                    },
-                    mustBeGreaterThanEqual: {
-                            jersey: [15*4, 'uint32', 1], // offset, type, value
-                            height: [42*4, 'uint32', 1], // offset, type, value
-                            maxhr: [43*4, 'uint32', 1], // offset, type, value
+                        ],
+                        mustBeGreaterThanEqual: {
+                                // jersey: [15*4, 'uint32', 1], // offset, type, value
+                                height: [42*4, 'uint32', 1], // offset, type, value
+                                maxhr: [43*4, 'uint32', 1], // offset, type, value
+                        },
                     },
                     addressOffset: 0
                 }
